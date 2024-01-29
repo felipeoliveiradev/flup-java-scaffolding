e709c5f0e5dad462a66cade3dc2c330d031c5883
Felipe Oliveira <felipeoliveiradeveloper@gmail.com>
2024-01-29 06:57:33 +0000

    Delete .history directory

--- .history/src/main/resources/db/migration/V5__category_20240128092208.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE category (
-    id VARCHAR(36) NOT NULL PRIMARY KEY,
-    name VARCHAR(255),
-    description VARCHAR(2000) NOT NULL,
-    active BOOLEAN NOT NULL DEFAULT TRUE,
-    created_at DATETIME(6) NOT NULL,
-    updated_at DATETIME(6) NOT NULL,
-    deleted_at DATETIME(6) NULL
-    )
